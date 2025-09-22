1880277023f0007b7ecc501cedde28595ea3b7b8
Antonio Marcos Sousa de Oliveira <antonioso@baraodemaua.br>
2025-09-22 16:54:07 -0300

    feat: ajuste no visual

--- src/app/componente/componente.ts
+++ src/app/componente/componente.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// Angular Material
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+
 import { Dados, Service } from '../services/service';
-import { FormsModule } from '@angular/forms';
 import { finalize } from 'rxjs';
 import { Map } from '../map/map';
 
@@ -12,11 +18,16 @@ import { Map } from '../map/map';
   selector: 'app-componente',
   standalone: true,
   imports: [
-    FormsModule,
     CommonModule,
+    FormsModule,
+    // Material
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    // seu mapa
     Map
   ],
   templateUrl: './componente.html',
@@ -24,44 +35,43 @@ import { Map } from '../map/map';
 })
 export class Componente { 
   local: string = '';
-  dados? : Dados;
+  dados?: Dados;
   loading: boolean = false;
   erro: string = '';
 
-  constructor(private service : Service, private cdr : ChangeDetectorRef){}
+  constructor(private service: Service, private cdr: ChangeDetectorRef) {}
 
-  pesquisarLocal(){
-  console.log('Pesquisando local:', this.local);
-  this.service.GetLatitude(this.local).subscribe({
-    next: (res) => {
-      console.log('Coordenadas encontradas:', res[0].lat, res[0].lon);
-      this.pegarDadosClima(res[0].lat, res[0].lon)
-    },
-    error: (err) => {
-      console.error('Erro ao buscar coordenadas:', err);
-      this.erro = "Cidade não encontrada";
-    }
-  })
-}
+  pesquisarLocal() {
+    console.log('Pesquisando local:', this.local);
+    this.service.GetLatitude(this.local).subscribe({
+      next: (res) => {
+        console.log('Coordenadas encontradas:', res[0].lat, res[0].lon);
+        this.pegarDadosClima(res[0].lat, res[0].lon)
+      },
+      error: (err) => {
+        console.error('Erro ao buscar coordenadas:', err);
+        this.erro = "Cidade não encontrada";
+      }
+    });
+  }
 
-  pegarDadosClima(lat: number, lon: number){
+  pegarDadosClima(lat: number, lon: number) {
     this.loading = true;
     this.erro = '';
     this.service.GetClima(lat, lon).pipe(
-      finalize(()=> {
-        this.loading=false;
+      finalize(() => {
+        this.loading = false;
         this.cdr.detectChanges();
       })
-    )
-    .subscribe({
+    ).subscribe({
       next: (res) => {
         this.dados = res;
       },
       error: (err) => {
-        this.erro = "Cidade não encontrada ou erro na API: " + err.Message
+        this.erro = "Cidade não encontrada ou erro na API: " + err.Message;
         this.loading = false;
       }
-    })
+    });
   }
 
   getIconUrl(icon: string) {
